a99d801cf9d90112bec80d7f62c7c52ec7729a43
Dulip Sameera <dsgitrepo101@gmail.com>
2025-03-03 10:26:35 +0530

    Change some styles and add the cv file

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector('#menu');
 const nav = document.querySelector('.links');
+const links = nav.querySelectorAll('a');
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     nav.classList.toggle('active')
 }
+
+// Close the menu when any link is clicked
+links.forEach(link => {
+    link.onclick = () => {
+        menu.classList.remove('bx-x');
+        nav.classList.remove('active');
+    };
+});
